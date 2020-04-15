65233b3cbb58b031d2cedf130a1c758bdff8e9a3
AconLight <w.mosienski@gmail.com>
2020-04-15 15:33:43 +0200

    files structure

---
+++ Assets/Scripts/Board/Grid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    List<GridElement> GridElements;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GridElements = new List<GridElement>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
