5b73142012fd699742c4e540729aab347486dfbb
Wiktor <wiktor.muraszko@wp.pl>
2020-04-15 18:09:44 +0200

    ZOne + unit

--- Assets/Scripts/Units/UnitBase.cs
+++ Assets/Scripts/Units/UnitBase.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitBase : MonoBehaviour
-{
+{   [Range(1,10)]
+    public int maxHP;
+    private int currentHP;
+    [Range(1,10)]
+    public int basicIniative;
+    private int currentIniative;
+    public List<InfluenceZoneBase> influenceZones;
+
     // Start is called before the first frame update
     void Start()
     {
