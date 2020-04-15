b92917078bea90ce4fcc792210d6c2431527d54d
AconLight <w.mosienski@gmail.com>
2020-04-15 16:11:01 +0200

    grid setup

--- Assets/Scripts/Board/Grid.cs
+++ Assets/Scripts/Board/Grid.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-
-    List<GridElement> GridElements;
+    public GameObject gridElementPrefab;
+    public int size = 10;
+    List<List<GameObject>> GridElements;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        GridElements = new List<GridElement>();
-        
+        GridElements = new List<List<GameObject>>();
+        for (int z = -size/2; z < size/2; z++) {
+            List<GameObject> temp = new List<GameObject>();
+            for (int x = -size/2; x < size/2; x++) {
+                GameObject go = Instantiate (gridElementPrefab, new Vector3(transform.position.x + x,transform.position.y, transform.position.z + z) , Quaternion.identity, this.transform);
+                go.GetComponent<GridElement>().x = x;
+                go.GetComponent<GridElement>().z = z;
+                temp.Add(go);
+            }
+            GridElements.Add(temp);
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Board/GridElement.cs
+++ Assets/Scripts/Board/GridElement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GridElement : MonoBehaviour
 {
+    public int x, z;
+    private Grid grid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        grid = this.transform.parent.GetComponent<Grid>();
     }
 
     // Update is called once per frame
