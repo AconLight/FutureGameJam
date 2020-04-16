080cbe1d9de52355e6dd5177f28b5c99f3c0c95a
Wiktor <wiktor.muraszko@wp.pl>
2020-04-16 16:29:23 +0200

    GAmeEngine

---
+++ Assets/Scripts/Logic/GameEngine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState { START, PLAYER1TURN, PLAYER2TURN, BATTLETURN, LOST }
+public class GameEngine : MonoBehaviour
+{
+    public GameState state;
+    public GameObject gridManagerScript;
+    private List<GameObject> allUnits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = GameState.START;
+    }
+
+    void detectUnits()
+    {
+         
+    }
+}
