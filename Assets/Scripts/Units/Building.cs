fae8bbaa63f451570922620a18626edac1c4cc27
Wiktor <wiktor.muraszko@wp.pl>
2020-04-17 19:56:57 +0200

    moje głupoty dodatkowa scena zmaniy z wczoraj nie wypushowane

--- Assets/Scripts/Units/Building.cs
+++ Assets/Scripts/Units/Building.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildingBehaviour : MonoBehaviour
+public class Building : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
