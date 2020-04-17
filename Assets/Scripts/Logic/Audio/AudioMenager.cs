76256bfe7b9e3efa697b8b2f9e0f0cccd8ff12bf
Piotr Ste <pstefanski@gmail.com>
2020-04-17 16:01:04 +0200

    AudioMEnager
    XD
    
    
    nowa muyczka

---
+++ Assets/Scripts/Logic/Audio/AudioMenager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AudioMenager : MonoBehaviour
+{
+
+    public GameObject bgMusicPrefab;
+    private GameObject bgMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgMusic = Instantiate(bgMusicPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        bgMusic.GetComponent<AudioSource>().Play((ulong)0.2);
+
+        //bgMusic.Play(0.5);
+       // audio.clip = otherClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
