21243806d505492fede59d1d3a0b8e0f68e400ea
Piotr Ste <pstefanski@gmail.com>
2020-04-18 20:15:02 +0200

    UnitDescription

---
+++ Assets/Scripts/Units/UnitDescription.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitDescription
+{
+    string cardDescription;
+    string cardName;
+
+    public UnitDescription()
+    {
+        //lul
+    }
+
+    public void setCardDescription(string name, string description)
+    {
+        cardName = name;
+        cardDescription = description;
+    }
+
+    public void setCardDescription(string description)
+    {
+        cardDescription = description;
+    }
+
+    public void setCardName(string name)
+    {
+        cardName = name;
+    }
+
+}
